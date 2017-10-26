b57c3d63f78075bd6e88c9109a7966633748633b
Abhay Bothra <bothra90@users.noreply.github.com>
2017-10-25 23:57:10 -0700

    Remove duplicated code

--- functional/src/iterators/mod.rs
+++ functional/src/iterators/mod.rs
@@ -109,34 +109,3 @@ fn using_other_iterator_trait_methods() {
                                  .sum();
     assert_eq!(18, sum);
 }
-
-#[derive(PartialEq, Debug)]
-struct Shoe {
-    size: i32,
-    style: String,
-}
-
-fn shoes_in_my_size(shoes: Vec<Shoe>, shoe_size: i32) -> Vec<Shoe> {
-    shoes.into_iter()
-        .filter(|s| s.size == shoe_size)
-        .collect()
-}
-
-#[test]
-fn filters_by_size() {
-    let shoes = vec![
-        Shoe { size: 10, style: String::from("sneaker") },
-        Shoe { size: 13, style: String::from("sandal") },
-        Shoe { size: 10, style: String::from("boot") },
-    ];
-
-    let in_my_size = shoes_in_my_size(shoes, 10);
-
-    assert_eq!(
-        in_my_size,
-        vec![
-            Shoe { size: 10, style: String::from("sneaker") },
-            Shoe { size: 10, style: String::from("boot") },
-        ]
-    );
-}
