37dfb33db7a22d8014d9ffd81edd3f6279ba2e75
Abhay Bothra <bothra90@users.noreply.github.com>
2018-12-26 21:55:32 -0800

    Move library module to 2018 edition

--- library/src/lib.rs
+++ library/src/lib.rs
@@ -15,13 +15,21 @@ mod outermost {
 
     pub mod inside {
         pub fn inner_function() {
+            // Following does not work
             // ::outermost::middle_function();
+            // We can either refer to parent module as super
+            super::middle_function();
+            // Or, provide a full path starting with ::crate
             crate::outermost::middle_secret_function();
+            // Functions in the same module can be referenced without any extra qualification.
+            secret_function();
         }
 
         pub fn secret_function() {}
     }
 }
+
+mod outer;
 
 fn try_me() {
     outermost::middle_function();
